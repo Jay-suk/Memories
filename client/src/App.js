7c27213849485a949f7e5fbe7b10a049990f093f
jay suk <jaykumargupta2307@gmail.com>
2023-05-18 15:42:30 +0530

    till-layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,23 @@
 //files imported -- posts.js and form.js from components , getPosts method from /actions/posts
-import React , { useState, useEffect } from "react";
-//importing material-ui stuff for styling
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-//importing the Posts and Form component
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-//importing getPosts method
-import { getPosts } from './actions/posts'
-
-//image import
-import memories from './images/memories.png';
-import useStyles from './styles';
-
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 //layout of our app with material-ui styling
 const App = () => {
-
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  //dispatch(getPosts()) whenever there is a change in [dispatch](change in redux store)
-  useEffect( () => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-  
-  return (
+  <BrowserRouter>
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-          <img className={classes.image} src={memories} alt="memories" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId}/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
     </Container> 
-  );
+  </BrowserRouter>
 };
 
 //exporting this component as default to index.js
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 //files imported -- app.js , /reducers/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
@@ -13,12 +13,12 @@ import './index.css';
 
 //create a redux store and apply the thunk middleware to handle side-effects and asynchronous operation
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //render the App component into the HTML element with id root
 //provider component -- to wrap the react application and provide acces to redux store
-ReactDOM.render(
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
     );
