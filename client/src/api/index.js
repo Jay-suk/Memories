c9cfa0c4b59f563a168a4a7d2b92702713ccda09
jay suk <jaykumargupta2307@gmail.com>
2023-05-31 11:05:02 +0530

    backend deployed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,9 +2,10 @@
 import axios from 'axios';
 
 //this url points to the backend route
+//https://memories-jay.onrender.com/
+//const API = axios.create( {baseURL : 'http://localhost:5000'} );
 
-const API = axios.create( {baseURL : 'http://localhost:5000'} );
-
+const API = axios.create( {baseURL : 'https://memories-jay.onrender.com'} );
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
